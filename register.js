cda4b55ad9974b2a282f6825e4861b9e8c329f0f
SebastianAlexAndreyR <andreysebastian11.11@gmail.com>
2024-03-05 15:59:47 +0800

    added html and js file

---
+++ register.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+console.error("Error!HAHA");
+console.warn("Warning!HEHE");
+
+// Variables
+// String = "Words"
+// Number = 123, 456e. 78.90
+// Boolean = true or false
+// Object = ["apple", "banana"], [1,2,3,4...]
+
+//console.log(typeof "123");
+
+// Declaration of Variable
+
+let name = "Alex", // String
+    lastname = "Sebastian", // String
+    age = 19, // Number
+    IsStudent = true, // Boolean 
+    hobbies = ["listning music", "play games"], // Object
+run = function () {
+    console.log("Im dying.");
+}; // Function
+console.log(name, age, IsStudent, hobbies);
+console.table(hobbies);
+console.log("My name is " + name + "" + lastname);
+console.log(`My name is  ${name} ${lastname}`);
